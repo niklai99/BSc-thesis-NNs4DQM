a28b176e386629a3731a7be834405f799ea0f59e
Nicolo Lai <nlai@t2-ui-12.pd.infn.it>
2021-09-11 14:33:36 +0200

    updates on drift_time analysis and new StreamReader class

---
+++ FETCH_DATA/readTXT.py
@@ -0,0 +1,35 @@
+import argparse
+from ReadStream import StreamReader
+
+
+def argParser():
+    '''gestisce gli argomenti passati da linea di comando'''
+    
+    parser = argparse.ArgumentParser() 
+    parser.add_argument('-o','--output', type=str, default='/lustre/cmswork/nlai/DATA/', help="output directory")
+    parser.add_argument('-run','--run', type=int, default=1220, help="run number")
+    parser.add_argument('-n','--nfiles', type=int, default=-1, help="number of files")
+    
+    return parser.parse_args()
+
+
+def main(args):
+
+    RUNNUMBER = args.run
+    OUT_PATH = args.output
+    N_FILES = args.nfiles
+    
+    reader = StreamReader(RUNNUMBER, OUT_PATH, N_FILES)
+    
+    reader.readStream()
+    reader.cleanData()
+    reader.saveData()
+    
+    print('\n\nExiting...\n\n')
+    
+    return
+
+
+if __name__ == "__main__":
+    args = argParser()
+    main(args)
